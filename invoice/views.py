315b6562bb1bab473f83c3b591509c693b4eb9af
sriramadari <lakhsmisriramadari1427@gmail.com>
2024-01-03 18:46:37 +0530

    changes

--- invoice/views.py
+++ invoice/views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets
 from .models import Invoice, InvoiceDetail
-from .serializers import InvoiceSerializer, InvoiceDetailSerializer
+from .serializers import InvoiceSerializer, InvoiceDetailSerializer,InvoiceUpdateSerializer,InvoiceDetailUpdateSerializer
 
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
+    def get_serializer_class(self):
+        if self.action == 'update':
+            return InvoiceUpdateSerializer
+        return InvoiceSerializer
 
 class InvoiceDetailViewSet(viewsets.ModelViewSet):
     queryset = InvoiceDetail.objects.all()
     serializer_class = InvoiceDetailSerializer
+    def get_serializer_class(self):
+        if self.action == 'update':
+            return InvoiceDetailUpdateSerializer
+        return InvoiceSerializer
